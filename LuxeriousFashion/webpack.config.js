838c8606fbc8f5530abb6827696a8aa43b019b52
Ponkratsyeu Yauheni <eponkratyev@tula.co>
2022-06-18 22:52:52 +0300

    feat: Add webpack to project

---
+++ LuxeriousFashion/webpack.config.js
@@ -0,0 +1,117 @@
+const HTMLWebpackPlugin = require('html-webpack-plugin')
+const {CleanWebpackPlugin} = require('clean-webpack-plugin')
+// const FaviconsWebpackPlugin = require('favicons-webpack-plugin')
+const MiniCssExtractPlugin = require('mini-css-extract-plugin')
+const CssMinimizerPlugin = require("css-minimizer-webpack-plugin")
+const TerserWebpackPlugin = require('terser-webpack-plugin')
+
+const webpack = require("webpack");
+const path = require('path');
+const assert = require("assert");
+
+const isDev = process.env.NODE_ENV === 'development';
+const isProd = process.env.NODE_ENV !== 'development';
+const NODE_ENV = process.env.NODE_ENV || 'development';
+
+const filename = ext => isDev ? `[name].${ext}`:`[name].[hash].${ext}`
+const cssLoaders = preproc=>{
+    const use=[{
+        loader: MiniCssExtractPlugin.loader
+    }, 'css-loader']
+    if(preproc){
+        use.push(`${preproc.toLowerCase()}-loader`)
+    }
+    return use;
+}
+
+const plugins = [
+    new HTMLWebpackPlugin({
+        title: "Test title",
+        template: "./index.html",
+        minify:{
+            collapseWhitespace: isProd,
+        }
+    }),
+    new CleanWebpackPlugin(),   //Очистка папки dist перед каждой новой сборкой
+    // new FaviconsWebpackPlugin('./favicon.png'),
+    new MiniCssExtractPlugin({
+        filename: filename('css')
+    }),
+
+];
+
+if (isDev) {
+    plugins.push(new webpack.HotModuleReplacementPlugin());
+}
+
+
+module.exports = {
+    mode: "development",
+    context: path.resolve(__dirname, 'src'),
+    entry: {
+        main: './script.js',
+        // analytics: './analytics.js'
+    },
+    output: {
+        path: path.resolve(__dirname, 'dist'),
+        filename: filename('js'),
+        assetModuleFilename: "assets/[name][ext]",
+    },
+    watch: this.mode === 'development',   //Автом. пересборка проекта при изменениях
+    watchOptions: {
+        aggregateTimeout: 100       //Задание таймаута перед пересборкой проекта
+    },
+    plugins,
+    module: {
+        rules: [
+            {
+                test: /\.css$/,
+                use: cssLoaders()
+            },
+            {
+                test: /\.less$/,
+                use: cssLoaders('less')
+            },
+            {
+                test: /\.s[ac]ss$/,
+                use: cssLoaders('sass')
+            },
+            {
+                test: /\.(?:png|jpg|svg|gif)$/i,
+                type: "asset/resource",
+            },
+            {
+                test: /\.(ttf|woff|woff2|eot)/,
+                type: "asset/resource",
+            },
+            {
+                test: /\.xml$/,
+                use: ['xml-loader']
+            },
+            {
+                test: /\.csv$/,
+                use: ['csv-loader']
+            }
+        ]
+    },
+    resolve: {
+        extensions: ['.ts', '.js'],
+        alias: {
+            '@assets': path.resolve(__dirname, 'src/assets')
+        }
+    },
+    optimization: {
+        splitChunks: {
+            chunks: "all"   //Позволяет перенести общий код в отдельный файл (для оптимизации)
+        },
+        minimizer: [
+            new CssMinimizerPlugin(),      //Оптимизация CSS
+            new TerserWebpackPlugin()   //Оптимизация JavaScript
+        ],
+        minimize: isProd    //Разрешить оптимизацию в случае prodaction
+    },
+    devServer: {
+        port: 4300,
+        // hot: isDev
+    }
+};
